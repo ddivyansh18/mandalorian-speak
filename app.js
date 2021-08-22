45b9e07271409aec1c8e35d6589ee13adaacb0b4
Divyansh Dixit <ddivyansh18@gmail.com>
2021-08-23 03:21:17 +0530

    Added fetch call and CSS

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var inputTextarea = document.querySelector("textarea")
 var outputDiv = document.querySelector(".output")
+var btnTranslate = document.querySelector(".btn")
 
-console.log(inputTextarea);
-console.log(outputDiv);
+var serverURL = "https://api.funtranslations.com/translate/mandalorian.json";
 
+function getTranslationURL(inputText) {
+    return serverURL + "?" + "text" + "=" + inputText;
+}
+
+function clickHandler() {
+    
+    inputText = inputTextarea.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(response => outputDiv.innerText = response.contents.translated)
+    .catch(error => console.log(error))
+}
+
+btnTranslate.addEventListener("click",clickHandler);
